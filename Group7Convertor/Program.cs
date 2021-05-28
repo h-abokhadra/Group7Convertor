d5975f2806a22488abfbacb63a4dcb7a158cdc78
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-05-28 15:20:07 -0400

    Add project files.

---
+++ Group7Convertor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Hoda - Riyadh - Vlada - Kimberly
+namespace Group7Convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
