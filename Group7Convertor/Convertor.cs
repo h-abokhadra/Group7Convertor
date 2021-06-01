0cfb623292a10e2a9d9335cc2598004c88a9c734
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-05-31 22:19:29 -0400

    commening

--- Group7Convertor/Convertor.cs
+++ Group7Convertor/Convertor.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-//Hoda - Riyadh - Vlada - Kimberly
+//This program is for creating a convertor application.
+//Group7: Hoda - Riyadh - Vlada - Kimberly
 namespace Group7Convertor
 {
     class Convertor
