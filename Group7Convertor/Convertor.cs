1ff0fae2f6f9d26e82dfafe26c8f075b35bc754b
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-11 20:19:57 -0400

    test

--- Group7Convertor/Convertor.cs
+++ Group7Convertor/Convertor.cs
@@ -26,7 +26,43 @@ namespace Group7Convertor
             Console.WriteLine("Press 11 for Log \n");
             Console.WriteLine("Press 12 for Ln \n");
             Console.WriteLine("Press 13 for SquareRoot\n");
-     
+
+            int action = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 1st input");
+            int input_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd input");
+            int input_2 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            switch (action)
+            {
+                case 1:
+                    {
+                        result = ConvertorTest.Addition(input_1, input_2);
+                        break;
+                    }
+                case 2:
+                    {
+                        result = Subtraction(input_1, input_2);
+                        break;
+                    }
+                case 3:
+                    {
+                        result = Multiplication(input_1, input_2);
+                        break;
+                    }
+                case 4:
+                    {
+                        result = Division(input_1, input_2);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Wrong action!! try again");
+                    break;
+            }
+            Console.WriteLine("The result is {0}", result);
+            Console.ReadKey();
         }
+
     }
-}
+    }
+
