7818c341053336ab4d27046411d1714e37035f43
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-11 20:06:39 -0400

    working on the main

--- Group7Convertor/Convertor.cs
+++ Group7Convertor/Convertor.cs
@@ -11,7 +11,19 @@ namespace Group7Convertor
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome, please choose a command: \n");
+            Console.WriteLine("Press 1 for Addition \n");
+            Console.WriteLine("Press 2 for Subtraction \n");
+            Console.WriteLine("Press 3 for Multiplication\n");
+            Console.WriteLine("Press 4 for Division \n");
+            Console.WriteLine("Press 5 for Addition \n");
+            Console.WriteLine("Press 6 for Subtraction \n");
+            Console.WriteLine("Press 7 for Multiplication\n");
+            Console.WriteLine("Press 8 for Addition \n");
+            Console.WriteLine("Press 9 for Subtraction \n");
+            Console.WriteLine("Press 10 for Multiplication\n");
+            Console.WriteLine("Press 11 for Division \n");
+            Console.WriteLine("Press 12 for Division \n");
 
         }
     }
