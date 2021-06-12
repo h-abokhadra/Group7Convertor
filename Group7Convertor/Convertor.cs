380f1d76d5712214274e41ced0267333b091238d
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-11 20:12:41 -0400

    update

--- Group7Convertor/Convertor.cs
+++ Group7Convertor/Convertor.cs
@@ -15,22 +15,18 @@ namespace Group7Convertor
 
             Console.WriteLine("Press 1 for Addition \n");
             Console.WriteLine("Press 2 for Subtraction \n");
-            Console.WriteLine("Press 3 for Multiplication\n");
+            Console.WriteLine("Press 3 for Multiplication \n");
             Console.WriteLine("Press 4 for Division \n");
-            Console.WriteLine("Press 5 for Addition \n");
-            Console.WriteLine("Press 6 for Subtraction \n");
-            Console.WriteLine("Press 7 for Multiplication\n");
-            Console.WriteLine("Press 8 for Addition \n");
-            Console.WriteLine("Press 9 for Subtraction \n");
-            Console.WriteLine("Press 10 for Multiplication\n");
-            Console.WriteLine("Press 11 for Division \n");
-            Console.WriteLine("Press 12 for Division \n");
-            Console.WriteLine("Press 13 for Multiplication\n");
-            Console.WriteLine("Press 14 for Addition \n");
-            Console.WriteLine("Press 15 for Subtraction \n");
-            Console.WriteLine("Press 16 for Multiplication\n");
-            Console.WriteLine("Press 17 for Division \n");
-            Console.WriteLine("Press 18 for Division \n");
+            Console.WriteLine("Press 5 for Modulus \n");
+            Console.WriteLine("Press 6 for Factorial \n");
+            Console.WriteLine("Press 7 for Absolute \n");
+            Console.WriteLine("Press 8 for Power \n");
+            Console.WriteLine("Press 9 for Exponential \n");
+            Console.WriteLine("Press 10 for DividedByX \n");
+            Console.WriteLine("Press 11 for Log \n");
+            Console.WriteLine("Press 12 for Ln \n");
+            Console.WriteLine("Press 13 for SquareRoot\n");
+     
         }
     }
 }
